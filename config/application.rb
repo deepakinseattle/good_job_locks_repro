3df97f1b49d87cf4331efd8a7ba9a6197d4d33c1
Deepak Kumar <deepakinseattle@gmail.com>
2020-12-15 14:56:45 -0800

    Configure execution mode for good job

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module GoodJobLocksRepro
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.active_job.queue_adapter = :good_job
+    config.active_job.queue_adapter = GoodJob::Adapter.new(execution_mode: :external)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
